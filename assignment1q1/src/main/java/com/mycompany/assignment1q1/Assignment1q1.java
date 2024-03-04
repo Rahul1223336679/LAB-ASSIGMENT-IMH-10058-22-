668d5b7a1ace26eb6d1dd0bf59f390de571f0a52
Rahul1223336679 <158079499+Rahul1223336679@users.noreply.github.com>
2024-03-04 21:42:07 +0530

    Update Assignment1q1.java

--- assignment1q1/src/main/java/com/mycompany/assignment1q1/Assignment1q1.java
+++ assignment1q1/src/main/java/com/mycompany/assignment1q1/Assignment1q1.java
@@ -1,35 +1,27 @@
 import java.util.Scanner;
-
-public class SearchArray {
+public class Assigment1q1{
     public static void main(String[] args) {
+        int[] array = createArray();
+        int numberOfElement = array.length;
+        System.out.print("Array: ");
+        for (int i = 0; i < numberOfElement; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Number of elements: " + numberOfElement);
+    }
+    public static int[] createArray() {
         Scanner scanner = new Scanner(System.in);
-
-        // Create an array (you can modify this array or take input from the user as in the previous example)
-        int[] array = {10, 20, 30, 40, 50};
-
-        // Ask user for the element to search
-        System.out.print("Enter the element to search: ");
-        int element = scanner.nextInt();
-
-        // Perform linear search
-        boolean found = false;
-        int index = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == element) {
-                found = true;
-                index = i;
-                break;
-            }
+        System.out.print("Enter the size of the array: ");
+        int sizeOfArray = scanner.nextInt();
+        
+        int[] array = new int[sizeOfArray];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < sizeOfArray; i++) {
+            array[i] = scanner.nextInt();
         }
-
-        // Display the result
-        if (found) {
-            System.out.println("Element " + element + " found at index " + index);
-        } else {
-            System.out.println("Element " + element + " not found in the array");
-        }
-
-        // Close the scanner
-        scanner.close();
+        
+        return array;
     }
 }
+
